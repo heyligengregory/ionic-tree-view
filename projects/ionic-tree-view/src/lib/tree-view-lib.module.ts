3f744b2f87c1ba5aed46d9baebf098fe6d80d5db
HEYLGREG <gregory.heyligen@computerland.be>
2019-09-22 22:49:05 +0200

    Add forRoot & injection

--- projects/ionic-tree-view/src/lib/tree-view-lib.module.ts
+++ projects/ionic-tree-view/src/lib/tree-view-lib.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { TreeViewComponent } from './tree-view/tree-view.component';
 import { TreeViewItemsComponent } from './tree-view-items/tree-view-items.component';
 import { TreeViewItemComponent } from './tree-view-item/tree-view-item.component';
+import { TreeViewDataService } from './tree-view-data.service';
+import { TreeViewEventService } from './tree-view-event.service';
+import { TreeViewLibService } from './tree-view-lib.service';
 
 @NgModule({
     declarations: [
@@ -22,4 +25,15 @@ import { TreeViewItemComponent } from './tree-view-item/tree-view-item.component
         TreeViewItemComponent
     ]
 })
-export class TreeViewModule { }
+export class TreeViewModule { 
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: TreeViewModule,
+            providers: [
+                TreeViewDataService,
+                TreeViewLibService,
+                TreeViewEventService
+            ]
+        };
+    }
+}
